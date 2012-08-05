41b5192805b8604ac6cc0d139454a043eacb1e6b
Pietro Martinelli <amicofragile@gmail.com>
2012-08-05 20:13:53 +0200

    Added test for generic Item.canOverwrite(Item) method

--- src/test/groovy/it/unibs/ing/fp/traffic/CarTest.groovy
+++ src/test/groovy/it/unibs/ing/fp/traffic/CarTest.groovy
@@ -1,13 +1,26 @@
 package it.unibs.ing.fp.traffic
 
 import static org.junit.Assert.*;
+import org.junit.Test
+import org.junit.Before
 
 class CarTest {
-	@org.junit.Test
-	public void carMovesDown() throws Exception {
-		Item cyclist = new Car()
-		Position dest = cyclist.moveFrom([x: 3, y: 5] as Position)
+	def Item car
+	
+	@Before
+	def void initCar() {
+		car = new Car()
+	}
+	
+	@Test
+	def void carMovesDown() throws Exception {
+		Position dest = car.moveFrom([x: 3, y: 5] as Position)
 		Position expected = [x:3, y:7] as Position
 		assert expected == dest
+	}
+	
+	@Test
+	def void carCantOverwritePedestrian() {
+		assert !car.canOverwrite(new Pedestrian())
 	}
 }
